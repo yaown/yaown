a99d3f8fbc3ed3bb7958d8c4fbf2bf3d2a96d344
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2020-03-08 16:20:33 +0100

    diese File wird die Klassen für DB enthalten

---
+++ lib/db_classes.dart
@@ -0,0 +1,66 @@
+/*
+@MaxL
+08032020
+v.1
+*/
+
+import 'package:flutter/cupertino.dart';
+
+enum Currency {
+  EUR,
+  USD,
+  GBP
+}
+
+// Klasse der Accounts
+class Account{
+  //----------------------------------------------------------------------------region variables
+  final int account_id;
+  final String name;
+  double balance;
+  double start_date;
+  // ignore: non_constant_identifier_names
+  int fk_user_id; //foreign key
+  Type currency = Currency; // enum
+  //------------------------------------------------------------------------------endregion variables
+
+  //------------------------------------------------------------------------------region methods
+  double deposit(double amount) => balance = balance + amount;
+  double withdraw(double amount) => balance = balance - amount;
+  //------------------------------------------------------------------------------end region methods
+}
+
+//Klasse Transaktionen auf Konten
+// TRX = Transaction Business >> Accounting
+class Transaction {
+  //-------------------------------------------------------------------------------------- region Variables
+  final int trx_id;
+  int name;
+  double trx_amount;
+  Type currency = Currency; // enum
+  double trx_time; //Zeitpunkt der Transaktion
+  String geo_tag; //Location  of the device
+  String desciption;
+//----------------------------------------------------------------------------------- endregion Variables
+
+  //--------------------------------------------------------------------------------- region Initialization
+
+  //------------------------------------------------------------------------------ endregion Initialization
+}
+
+class Category extends Account{
+  //-------------------------------------------------------------------------------------- region Variables
+  final int category_id;
+  int fk_category_id; //parent
+  var sub_categories = new List<int>(); //child
+  //----------------------------------------------------------------------------------- endregion Variables
+}
+//Can be any kind of Document including Receipts
+class Document{
+  final int document_id;
+  String name;
+  Image document_image; //Repräsentation?
+}
+
+
+
